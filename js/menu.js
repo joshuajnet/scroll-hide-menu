05eb3d0512f2806496f8d1d198a69676a9410800
joshuajnet <joshuajohnson@Joshuas-MacBook-Air.local>
2016-06-17 09:53:33 -0400

    throttle
    added throttle snippet from underscore.js library to cut back on scroll
    events

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,44 @@
 $(document).ready(function() {
+	
+	//throttle
+	 
+	$.throttle = function(func, wait, options) {
+	    var context, args, result;
+	    var timeout = null;
+	    var previous = 0;
+	    if (!options) options = {};
+	    var later = function() {
+	      previous = options.leading === false ? 0 : $.now();
+	      timeout = null;
+	      result = func.apply(context, args);
+	      if (!timeout) context = args = null;
+	    };
+	    return function() {
+	      var now = $.now();
+	      if (!previous && options.leading === false) previous = now;
+	      var remaining = wait - (now - previous);
+	      context = this;
+	      args = arguments;
+	      if (remaining <= 0 || remaining > wait) {
+	        if (timeout) {
+	          clearTimeout(timeout);
+	          timeout = null;
+	        }
+	        previous = now;
+	        result = func.apply(context, args);
+	        if (!timeout) context = args = null;
+	      } else if (!timeout && options.trailing !== false) {
+	        timeout = setTimeout(later, remaining);
+	      }
+	      return result;
+	    };
+	  };		
+	
 
 //scroll hide	
 		var lastScrollTop = 0;		
 				    
-	    $(document).scroll(function(){
+	    $(document).on('scroll', $.throttle(function() {
 	        
 	        var stop = $(this).scrollTop();
 				        
@@ -28,6 +63,6 @@ $(document).ready(function() {
 	        } 
 	        lastScrollTop = stop;
 		        
-	    });
+	    }, 100));
 
 });
